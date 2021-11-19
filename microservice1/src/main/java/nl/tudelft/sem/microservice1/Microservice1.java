9bb20b1f247d5db9a950456648cc6c6c30db652b
Giulio Segalini <g.segalini@student.tudelft.nl>
2021-11-19 12:43:57 +0100

    checkstyle based on config/checkstyle.xml

--- microservice1/src/main/java/nl/tudelft/sem/microservice1/Microservice1.java
+++ microservice1/src/main/java/nl/tudelft/sem/microservice1/Microservice1.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Microservice1 {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Microservice1.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Microservice1.class, args);
+    }
 
 }
--- microservice2/src/main/java/nl/tudelft/sem/microservice2/Microservice2.java
+++ microservice2/src/main/java/nl/tudelft/sem/microservice2/Microservice2.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Microservice2 {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Microservice2.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Microservice2.class, args);
+    }
 
 }
