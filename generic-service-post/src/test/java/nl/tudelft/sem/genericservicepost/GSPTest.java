b67a0142d0b534ab1abaa50337c1abfa0dd98074
Giulio Segalini <g.segalini@student.tudelft.nl>
2021-11-29 11:58:09 +0100

    tests for application contexts

---
+++ generic-service-post/src/test/java/nl/tudelft/sem/genericservicepost/GSPTest.java
@@ -0,0 +1,14 @@
+package nl.tudelft.sem.genericservicepost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(classes = {GSP.class})
+public class GSPTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ users/src/test/java/nl/tudelft/sem/users/UsersTest.java
@@ -0,0 +1,14 @@
+package nl.tudelft.sem.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(classes = {Users.class})
+public class UsersTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
