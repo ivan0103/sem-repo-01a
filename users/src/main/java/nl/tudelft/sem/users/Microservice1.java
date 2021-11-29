c15b895ad625c35a466b8eae9dac097a1a904780
Giulio Segalini <g.segalini@student.tudelft.nl>
2021-11-29 11:27:35 +0100

    refactored name and added all microservices

---
+++ users/src/main/java/nl/tudelft/sem/users/Microservice1.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Microservice1.class, args);
+    }
+
+}
---
+++ users/src/test/java/nl/tudelft/sem/users/ExampleTest.java
@@ -0,0 +1,16 @@
+package nl.tudelft.sem.users;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testGettersSetters() {
+        Example exampleTest = new Example("name");
+        assertEquals("name", exampleTest.getName());
+        exampleTest.setName("newName");
+        assertEquals("newName", exampleTest.getName());
+    }
+}
