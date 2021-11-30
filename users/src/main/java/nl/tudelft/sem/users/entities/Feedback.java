d701a5d4415177b5c14489ad6a41ab583f1b58ec
Andrei Mânăstireanu <a.b.manastireanu@student.tudelft.nl>
2021-11-30 10:56:57 +0100

    Set up feedback entity

--- users/src/main/java/nl/tudelft/sem/users/entities/Feedback.java
+++ users/src/main/java/nl/tudelft/sem/users/entities/Feedback.java
@@ -33,6 +33,10 @@ public class Feedback {
     @Column(name = "rating")
     private Integer rating;
 
+    /**
+     * Default constructor.
+     */
+
     public Feedback() {
 
     }
@@ -53,37 +57,93 @@ public class Feedback {
         this.rating = rating;
     }
 
+    /**
+     * Getter for id.
+     *
+     * @return id of feedback
+     */
+
     public Long getId() {
         return this.id;
     }
+
+    /**
+     * Setter for id.
+     *
+     * @param id the new id
+     */
 
     public void setId(Long id) {
         //Doesn't do anything. Used only to suppress warnings
     }
 
+    /**
+     * Getter for text.
+     *
+     * @return the text of the feedback
+     */
+
     public String getText() {
         return text;
     }
+
+    /**
+     * Getter for user.
+     *
+     * @return the user that submitted the feedback
+     */
 
     public User getUser() {
         return user;
     }
 
+    /**
+     * Setter for user.
+     *
+     * @param user the new user
+     */
+
     public void setUser(User user) {
         //Doesn't do anything. Used only to suppress warnings
     }
+
+    /**
+     * Getter for rating.
+     *
+     * @return the rating
+     */
 
     public Integer getRating() {
         return rating;
     }
 
+    /**
+     * Setter for text.
+     *
+     * @param text the new text
+     */
+
     public void setText(String text) {
         this.text = text;
     }
 
+    /**
+     * Setter for rating.
+     *
+     * @param rating the new rating
+     */
+
     public void setRating(Integer rating) {
         this.rating = rating;
     }
+
+    /**
+     * Equals method.
+     *
+     * @param o the object used to compare the feedback
+     * @return true - the feedbacks represent the same instance
+     *         false - otherwise
+     */
 
     @Override
     public boolean equals(Object o) {
@@ -100,10 +160,22 @@ public class Feedback {
                 && Objects.equals(rating, feedback.rating);
     }
 
+    /**
+     * HashCode method - It generates a hashCode to feedback.
+     *
+     * @return a hashCode for each feedback
+     */
+
     @Override
     public int hashCode() {
         return Objects.hash(id, text, user, rating);
     }
+
+    /**
+     * toString method.
+     *
+     * @return a human friendly representation of the user
+     */
 
     @Override
     public String toString() {
