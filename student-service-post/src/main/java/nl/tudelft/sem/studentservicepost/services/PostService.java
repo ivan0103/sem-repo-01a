c6b9ca9a7c5756d055a084377e030acb45163c97
Giulio Segalini <g.segalini@student.tudelft.nl>
2021-12-01 17:37:07 +0100

    dummy method for editing with commented possible implementation

--- student-service-post/src/main/java/nl/tudelft/sem/studentservicepost/services/PostService.java
+++ student-service-post/src/main/java/nl/tudelft/sem/studentservicepost/services/PostService.java
@@ -1,6 +1,7 @@
 package nl.tudelft.sem.studentservicepost.services;
 
 import nl.tudelft.sem.studentservicepost.entities.Post;
+import nl.tudelft.sem.studentservicepost.exceptions.PostNotFoundException;
 import nl.tudelft.sem.studentservicepost.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,5 +26,23 @@ public class PostService {
         Post returned = postRepository.save(post);
         System.out.println(returned);
         return returned;
+    }
+
+    /**
+     * Edit post.
+     *
+     * @param post the post
+     * @return the post
+     * @throws PostNotFoundException id the post is not found in the database.
+     */
+    public Post editPost(Post post) {
+        return post;
+        // TODO change the value in the database
+        // TODO check if the user can actually edit this post, related to authentication
+/*        if (postRepository.existsById(post.getId())) {
+            return postRepository.save(post);
+        } else {
+            throw new PostNotFoundException();
+        }*/
     }
 }
