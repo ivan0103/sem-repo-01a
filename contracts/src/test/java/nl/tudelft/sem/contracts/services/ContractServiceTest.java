78b30b40ef65196606f31fbad4ffe3565cbde29e
Todor <T.Mladenovic@student.tudelft.nl>
2021-12-02 20:10:43 +0100

    Doing contractService tests
    The commit title says it all with this one.

--- contracts/src/test/java/nl/tudelft/sem/contracts/services/ContractServiceTest.java
+++ contracts/src/test/java/nl/tudelft/sem/contracts/services/ContractServiceTest.java
@@ -1,7 +1,81 @@
 package nl.tudelft.sem.contracts.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import nl.tudelft.sem.contracts.entities.Contract;
+import nl.tudelft.sem.contracts.repositories.ContractRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class ContractServiceTest {
+    Contract contract;
 
+    private ContractService underTest;
+
+    @Mock
+    private ContractRepository contractRepository;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new ContractService(contractRepository);
+
+        contract = new Contract(
+                1,
+                2,
+                "student",
+                "company",
+                LocalTime.of(6, 0),
+                14.5F,
+                LocalDate.of(2020, 6, 1),
+                LocalDate.of(2020, 9, 1)
+        );
+    }
+
+    @Test
+    void createTest() {
+        //setup
+        ArgumentCaptor<Contract> contractArgumentCaptor = ArgumentCaptor.forClass(Contract.class);;
+
+        //given
+        given(contractRepository.save(contract)).willReturn(contract);
+
+        //when
+        Contract testCase = underTest.create(contract);
+
+        //then
+        verify(contractRepository).save(contractArgumentCaptor.capture());
+        Contract capturedContract = contractArgumentCaptor.getValue();
+
+        assertThat(capturedContract).isEqualTo(contract);
+        assertThat(testCase).isEqualTo(contract);
+    }
+
+    @Test
+    void getContractTest() {
+        //setup
+        ArgumentCaptor<Long> idArgumentCaptor = ArgumentCaptor.forClass(Long.class);;
+
+        //given
+        given(contractRepository.findContractById(contract.getId())).willReturn(contract);
+
+        //when
+        Contract testCase = underTest.getContract(contract.getId());
+
+        //then
+        verify(contractRepository).findContractById(idArgumentCaptor.capture());
+        long capturedId = idArgumentCaptor.getValue();
+
+        assertThat(capturedId).isEqualTo(contract.getId());
+        assertThat(testCase).isEqualTo(contract);
+    }
 }
