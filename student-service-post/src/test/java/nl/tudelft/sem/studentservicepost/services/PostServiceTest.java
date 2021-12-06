a9077eb12a91b52e0e60609a3d832a6cecbcc584
Giulio Segalini <g.segalini@student.tudelft.nl>
2021-12-06 15:58:09 +0100

    tests are now unit tests with mocks

--- student-service-post/src/test/java/nl/tudelft/sem/studentservicepost/services/PostServiceTest.java
+++ student-service-post/src/test/java/nl/tudelft/sem/studentservicepost/services/PostServiceTest.java
@@ -1,12 +1,12 @@
 package nl.tudelft.sem.studentservicepost.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.math.BigDecimal;
 import java.util.Collection;
-import javax.transaction.Transactional;
+import java.util.List;
 import nl.tudelft.sem.studentservicepost.entities.Competency;
 import nl.tudelft.sem.studentservicepost.entities.Expertise;
 import nl.tudelft.sem.studentservicepost.entities.Post;
@@ -15,63 +15,133 @@ import nl.tudelft.sem.studentservicepost.exceptions.PostNotFoundException;
 import nl.tudelft.sem.studentservicepost.repositories.CompetencyRepository;
 import nl.tudelft.sem.studentservicepost.repositories.ExpertiseRepository;
 import nl.tudelft.sem.studentservicepost.repositories.PostRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Transactional
 class PostServiceTest {
+
+    final transient String expertise1 = "exp1";
+    final transient String competency1 = "comp1";
+    final transient String competency2 = "comp2";
+
 
     transient Post post;
     transient Post post1;
+    transient Post postReturned;
+    transient Post post1Returned;
+
+    transient Competency comp1;
+    transient Competency comp2;
 
 
-    @Autowired
+    transient Expertise exp1;
+
+
+    @InjectMocks
     transient PostService postService;
 
-    @Autowired
+    @Mock
     transient PostRepository postRepository;
 
-    @Autowired
+    @Mock
     transient CompetencyRepository competencyRepository;
 
-    @Autowired
+    @Mock
     transient ExpertiseRepository expertiseRepository;
 
 
     @BeforeEach
     void setup() {
+        comp1 = new Competency(competency1);
+        comp2 = new Competency(competency2);
+
+        exp1 = new Expertise("exp1");
         post = new Post();
-        post.setId(0L);
+        postReturned = new Post();
+
+        post.setId(1L);
+        postReturned.setId(1L);
+
         post.setAuthor("despacito");
+        postReturned.setAuthor("despacito");
+
         post.setPricePerHour(new BigDecimal("15.00"));
-        post.getCompetencySet().add(new Competency("comp1"));
-        post.getExpertiseSet().add(new Expertise("exp1"));
+        postReturned.setPricePerHour(new BigDecimal("15.00"));
+
+        postReturned.getCompetencySet().add(comp1);
+        postReturned.getCompetencySet().add(comp1);
+
+        postReturned.getExpertiseSet().add(exp1);
+        postReturned.getExpertiseSet().add(exp1);
+
 
         post1 = new Post();
-        post1.setId(12L);
+        post1Returned = new Post();
+
+        post1.setId(2L);
+        post1Returned.setId(2L);
+
         post1.setAuthor("lillolallo");
+        post1Returned.setAuthor("lillolallo");
+
         post1.setPricePerHour(new BigDecimal("100.00"));
-        post1.getCompetencySet().add(new Competency("comp2"));
-        post1.getExpertiseSet().add(new Expertise("exp1"));
+        post1Returned.setPricePerHour(new BigDecimal("100.00"));
 
-    }
+        post1.getCompetencySet().add(comp2);
+        post1Returned.getCompetencySet().add(comp2);
 
-    //@AfterEach
-    void reset() {
-        postRepository.deleteAllInBatch();
-        competencyRepository.deleteAllInBatch();
-        expertiseRepository.deleteAllInBatch();
+        post1.getExpertiseSet().add(exp1);
+        post1Returned.getExpertiseSet().add(exp1);
+
+
+        Mockito.when(postRepository.getPostById(1L)).thenReturn(postReturned);
+        Mockito.when(postRepository.getPostById(2L)).thenReturn(post1Returned);
+        Mockito.when(postRepository.save(any())).thenAnswer(i -> i.getArguments()[0]);
+
+        Mockito.when(postRepository.save(post)).thenReturn(postReturned);
+        Mockito.when(postRepository.save(post1)).thenReturn(post1Returned);
+        Mockito.when(expertiseRepository.existsById(any())).thenReturn(false, true);
+        Mockito.when(competencyRepository.existsById(any())).thenReturn(false, true);
+        Mockito.when(postRepository.existsById(any())).thenReturn(false);
+        Mockito.when(postRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(postRepository.existsById(2L)).thenReturn(true);
+
+        Mockito.when(postRepository.findAll()).thenReturn(List.of(postReturned, post1Returned));
+
+        Mockito.when(competencyRepository.getAllBySearchableStringContaining(competency1))
+            .thenReturn(
+                List.of(comp1)
+            );
+
+        Mockito.when(competencyRepository.getAllBySearchableStringContaining(competency2))
+            .thenReturn(
+                List.of(comp2)
+            );
+
+        Mockito.when(expertiseRepository.getAllBySearchableStringContaining(expertise1)).thenReturn(
+            List.of(exp1)
+        );
+
+        Mockito.when(postRepository.getAllByCompetencySetContaining(comp1))
+            .thenReturn(List.of(postReturned));
+        Mockito.when(postRepository.getAllByCompetencySetContaining(comp2))
+            .thenReturn(List.of(post1Returned));
+
+        Mockito.when(postRepository.getAllByExpertiseSetContaining(exp1))
+            .thenReturn(List.of(postReturned, post1Returned));
+
+
     }
 
     @Test
     void createPost() {
         Post tmp = postService.createPost(post);
-        assertThat(tmp).isEqualTo(post);
+        assertThat(tmp).isEqualTo(postReturned);
     }
 
     @Test
@@ -81,15 +151,12 @@ class PostServiceTest {
     }
 
     @Test
-    // TODO fix this
     void editPost() {
 
         Post tmp = postService.createPost(post);
-        postRepository.flush();
-        Competency newCompetency = new Competency("comp2");
 
         tmp.setPricePerHour(new BigDecimal("42.00"));
-        tmp.getCompetencySet().add(newCompetency);
+        tmp.getCompetencySet().add(comp2);
 
         Post edited = postService.editPost(tmp);
 
@@ -99,7 +166,7 @@ class PostServiceTest {
 
         assertThat(edited.getPricePerHour()).isEqualTo(new BigDecimal("42.00"));
 
-        assertThat(edited.getCompetencySet()).hasSize(2).containsOnlyOnce(newCompetency);
+        assertThat(edited.getCompetencySet()).hasSize(2).containsOnlyOnce(comp2);
 
     }
 
@@ -117,17 +184,20 @@ class PostServiceTest {
     }
 
     @Test
-    // TODO fix this
     void editPostFailingNetId() {
         Post tmp = postService.createPost(post);
-        Competency newCompetency = new Competency("comp2");
 
-        tmp.setPricePerHour(new BigDecimal("42.00"));
-        tmp.getCompetencySet().add(newCompetency);
+        Post toEdit = new Post();
+        toEdit.setId(tmp.getId());
+        Competency newCompetency = comp2;
 
-        tmp.setAuthor("anotherguy");
+        toEdit.setPricePerHour(new BigDecimal("42.00"));
+        toEdit.getCompetencySet().add(newCompetency);
 
-        assertThatThrownBy(() -> postService.editPost(tmp))
+        toEdit.setAuthor("anotherguy");
+
+
+        assertThatThrownBy(() -> postService.editPost(toEdit))
             .isInstanceOf(InvalidEditException.class);
 
     }
@@ -141,8 +211,8 @@ class PostServiceTest {
 
     @Test
     void testSearchByKeywordNotFound() {
-        Post tmp = postService.createPost(post);
-        Post tmp2 = postService.createPost(post1);
+        // postService.createPost(post);
+        // postService.createPost(post1);
         Collection<Post> result = postService.searchByKeyword("cOmp  4");
         assertThat(result).isEmpty();
     }
