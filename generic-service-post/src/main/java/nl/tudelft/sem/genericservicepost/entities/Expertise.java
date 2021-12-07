f906e08bbe1f0c5260345cf99472ff59b49cdc33
Marie Grundfør <m.l.grundfor@student.tudelft.nl>
2021-12-07 15:33:41 +0100

    Added constructors

--- generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/entities/Expertise.java
+++ generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/entities/Expertise.java
@@ -1,14 +1,23 @@
 package nl.tudelft.sem.genericservicepost.entities;
 
-import java.util.HashSet;
-import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Expertise {
+
+    public Expertise() {
+    }
+
+    public Expertise(String string) {
+        this.expertiseString = string;
+    }
+
     @Id
     @Column(name = "expertise")
     private String expertiseString;
@@ -30,5 +39,20 @@ public class Expertise {
 
     public void setGenericPostSet(Set<GenericPost> genericPostSet) {
         this.genericPostSet = genericPostSet;
+    }
+
+    @Override
+    public String toString(){ return expertiseString; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Expertise expertise = (Expertise) o;
+        return Objects.equals(expertiseString, expertise.expertiseString);
     }
 }
