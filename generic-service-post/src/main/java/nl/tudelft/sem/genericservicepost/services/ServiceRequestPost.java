8bfc9980e8f6ca2e8f19f6c1228123e7d5eba7f0
Ivan Virovski <i.virovski-1@student.tudelft.nl>
2021-12-11 14:59:42 +0100

    Added repository for the expertise and created method for creating a post for request of services.

--- generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/services/ServiceRequestPost.java
+++ generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/services/ServiceRequestPost.java
@@ -1,5 +1,8 @@
 package nl.tudelft.sem.genericservicepost.services;
 
+import nl.tudelft.sem.genericservicepost.entities.Expertise;
+import nl.tudelft.sem.genericservicepost.entities.GenericPost;
+import nl.tudelft.sem.genericservicepost.repositories.ExpertiseRepository;
 import nl.tudelft.sem.genericservicepost.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +16,27 @@ public class ServiceRequestPost {
     @Autowired
     transient PostRepository postRepository;
 
+    @Autowired
+    transient ExpertiseRepository expertiseRepository;
 
+    /**
+     * Create request post and save in database.
+     *
+     */
+    public GenericPost createPost(GenericPost genericPost){
+        genericPost.setId(null);
+        for (Expertise expertise : genericPost.getExpertiseSet()){
+            if (expertiseRepository.existsById(expertise.getExpertiseString())){
+                Expertise exp = expertiseRepository.getExpertiseByExpertiseString(expertise.getExpertiseString());
+                exp.getGenericPostSet().add(genericPost);
+                expertiseRepository.save(exp);
+            }
+            else {
+                expertiseRepository.save(expertise);
+            }
+        }
+        GenericPost returned = postRepository.save(genericPost);
+        System.out.println(returned);
+        return returned;
+    }
 }
