0d6278701da1083ce6fbb432f986bc623f11aafe
Ivan Virovski <i.virovski-1@student.tudelft.nl>
2021-12-13 17:29:31 +0100

    Added two exceptions and body for editing a generic Post.

--- generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/services/GenericPostService.java
+++ generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/services/GenericPostService.java
@@ -2,10 +2,14 @@ package nl.tudelft.sem.genericservicepost.services;
 
 import nl.tudelft.sem.genericservicepost.entities.Expertise;
 import nl.tudelft.sem.genericservicepost.entities.GenericPost;
+import nl.tudelft.sem.genericservicepost.exceptions.GenericPostNotFoundException;
+import nl.tudelft.sem.genericservicepost.exceptions.InvalidEditException;
 import nl.tudelft.sem.genericservicepost.repositories.ExpertiseRepository;
 import nl.tudelft.sem.genericservicepost.repositories.GenericPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.security.GeneralSecurityException;
 
 @Service
 public class GenericPostService {
@@ -40,4 +44,23 @@ public class GenericPostService {
         return genericPost;
     }
 
+    /**
+     * Edit generic post and save it.
+     *
+     * @param genericPost the generic Post
+     * @return the generic Post
+     * @throws GenericPostNotFoundException if id of the generic post was not found.
+     */
+    public GenericPost editGenericPost(GenericPost genericPost){
+        if (genericPostRepository.existsById(genericPost.getId())){
+            GenericPost edit = genericPostRepository.getGenericPostById(genericPost.getId());
+            if (edit.getAuthor().equals(genericPost.getAuthor())){
+                return genericPostRepository.save(genericPost);
+            }
+            else{
+                throw new InvalidEditException();
+            }
+        }
+        else throw new GenericPostNotFoundException();
+    }
 }
