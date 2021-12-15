8d49a3eecf91e66da443295c766e11fb798c6b8f
Andrei Mânăstireanu <a.b.manastireanu@student.tudelft.nl>
2021-12-15 13:36:44 +0100

    Fix controller returning 200OK when user or feedback not found

--- users/src/test/java/nl/tudelft/sem/users/services/UserServiceTest.java
+++ users/src/test/java/nl/tudelft/sem/users/services/UserServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import javassist.NotFoundException;
 import nl.tudelft.sem.users.entities.Company;
 import nl.tudelft.sem.users.entities.Feedback;
 import nl.tudelft.sem.users.entities.User;
@@ -55,7 +56,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testGetOneUser() {
+    public void testGetOneUser() throws NotFoundException {
         Mockito.when(userRepository.findById(user1.getNetID()))
                 .thenReturn(java.util.Optional.of(user1));
         assertEquals(user1, userService.getOneUser(user1.getNetID()));
@@ -74,7 +75,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testAddFeedback() {
+    public void testAddFeedback() throws NotFoundException {
         Feedback feedback = new Feedback(1L, "blah", 1, user1);
         Mockito.when(feedbackRepository.save(feedback)).thenReturn(feedback);
         User user3 = new UserFactory().createUser("b", "b", 0.2f, role);
@@ -92,7 +93,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testDeleteUser() {
+    public void testDeleteUser() throws NotFoundException {
         Mockito.when(userRepository.findById(user1.getNetID()))
                 .thenReturn(java.util.Optional.of(user1));
         Mockito.doNothing().when(feedbackRepository).deleteAll(user1.getFeedbacks());
@@ -101,7 +102,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void updateUser() {
+    public void updateUser() throws NotFoundException {
         User user3 = new UserFactory().createUser(user2.getNetID(),
                 "aaaaaa", user2.getRating(), ((Company) user2).getRole());
         user3.setName("aaaaaa");
@@ -112,7 +113,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testEditFeedback() {
+    public void testEditFeedback() throws NotFoundException {
         Feedback feedback = new Feedback(1L, "blah", 1, user1);
         User user3 = new UserFactory().createUser("b", "b", 0.2f, role);
         user3.addFeedback(feedback);
@@ -144,7 +145,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void testDeleteFeedback() {
+    public void testDeleteFeedback() throws NotFoundException {
         Feedback feedback = new Feedback(1L, "blah", 1, user1);
         User user3 = new UserFactory().createUser("b", "b", 0.2f, role);
         user3.addFeedback(feedback);
