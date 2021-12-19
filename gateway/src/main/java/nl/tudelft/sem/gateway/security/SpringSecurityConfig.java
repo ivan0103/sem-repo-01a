182324148556524fdd40523e4027d9bc31dce8cb
Todor <T.Mladenovic@student.tudelft.nl>
2021-12-19 22:57:43 +0100

    Adding Security Roles and Permissions
    This is my start at implementing spring security. There is still more that needs to be done but so far the roles are assigned permissions to actions using enums and spring security is enabled.

---
+++ gateway/src/main/java/nl/tudelft/sem/gateway/security/SpringSecurityConfig.java
@@ -0,0 +1,49 @@
+package nl.tudelft.sem.gateway.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public  SpringSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("Student")
+                .build();
+
+        return  new InMemoryUserDetailsManager(
+                annaSmithUser
+        );
+    }
+}
