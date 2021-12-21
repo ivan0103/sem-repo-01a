0b0b786a1e4f8fa79a13ca3dc9d67d2cbdb2ff47
Ivan Virovski <i.virovski-1@student.tudelft.nl>
2021-12-21 09:41:38 +0200

    Added in Generic Post controller. Will be moved later to a different controller

--- generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/controllers/GenericPostController.java
+++ generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/controllers/GenericPostController.java
@@ -2,6 +2,7 @@ package nl.tudelft.sem.genericservicepost.controllers;
 
 import javax.validation.Valid;
 import nl.tudelft.sem.genericservicepost.entities.GenericPost;
+import nl.tudelft.sem.genericservicepost.entities.StudentOffer;
 import nl.tudelft.sem.genericservicepost.services.GenericPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/genericpost")
@@ -22,5 +25,12 @@ public class GenericPostController {
     public ResponseEntity<GenericPost> createGenericPost(@Valid @RequestBody GenericPost post) {
         GenericPost savedPost = genericPostService.createGenericPost(post);
         return new ResponseEntity<>(savedPost, HttpStatus.CREATED);
+    }
+
+    // Will be moved to the Student Offer Controller than Marie made, after her merge.
+    @PostMapping("/getStudentsByPost")
+    public ResponseEntity<Set<StudentOffer>> getStudentsByPost(@Valid @RequestBody GenericPost post) {
+        Set<StudentOffer> result = genericPostService.retrieveStudentsInPost(post);
+        return new ResponseEntity<>(result, HttpStatus.FOUND);
     }
 }
