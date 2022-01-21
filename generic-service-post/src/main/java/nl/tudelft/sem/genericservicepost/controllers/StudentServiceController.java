2fc8ea8d21792b476cbe8b08db5fefb93830fe9c
Marie Grundfør <m.l.grundfor@student.tudelft.nl>
2022-01-21 10:18:33 +0100

    Changed get method to have GetMapping

--- generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/controllers/StudentServiceController.java
+++ generic-service-post/src/main/java/nl/tudelft/sem/genericservicepost/controllers/StudentServiceController.java
@@ -8,10 +8,7 @@ import nl.tudelft.sem.genericservicepost.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/studentgenericpost")
@@ -20,7 +17,7 @@ public class StudentServiceController {
     @Autowired
     private transient StudentService genericPostService;
 
-    @PostMapping("/getStudentsByPost")
+    @GetMapping("/getStudentsByPost")
     public ResponseEntity<Set<UserImpl>> getStudentsByPost(
             @Valid @RequestBody GenericPost post) {
         Set<UserImpl> result = genericPostService.retrieveStudentsInPost(post);
